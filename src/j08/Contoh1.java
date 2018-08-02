49c2db867ff941b217eabaa30b71666b2d9d2e59
ASUS <ASUS@LAPTOP-B71SN63T>
2018-08-02 07:47:14 +0700

    For While Do-While

---
+++ src/j08/Contoh1.java
@@ -0,0 +1,36 @@
+package j08;
+
+
+public class Contoh1 {
+    public static void main(String[] args){
+        //PERULANGAN FOR
+   int x;
+   for(x = 1; x<=5; x++)
+   {
+       System.out.println("SMK Telkom Malang");
+   }    
+    
+    //PENGULANGAN WHILE
+   int i = 1;
+   while(i <= 5)
+   {
+    System.out.println("SMK Telkom Malang");
+    i++;
+   }
+   
+   //PENGULANGAN DO WHILE
+   int j = 1;
+   do
+   {
+       System.out.println("SMK Telkom Malang");
+       j++;
+   }
+   while(j <= 5);
+    
+    
+    
+    
+    
+    }
+   
+}
